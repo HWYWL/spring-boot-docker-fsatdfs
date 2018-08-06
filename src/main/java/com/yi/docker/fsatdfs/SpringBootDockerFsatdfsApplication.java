3b911b0581d012655ddae5bd60782fd7f080e571
YI <18819478773@163.com>
2018-08-06 22:40:29 +0800

    :tada: 初始化提交

---
+++ src/main/java/com/yi/docker/fsatdfs/SpringBootDockerFsatdfsApplication.java
@@ -0,0 +1,12 @@
+package com.yi.docker.fsatdfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerFsatdfsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerFsatdfsApplication.class, args);
+    }
+}
